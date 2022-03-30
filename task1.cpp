ffb271a147ef01ef493fbab5658d53f94c8a72b4
georgyteterin <teterin.normal@gmail,com>
2022-03-30 23:01:08 +0300

    tasks 1,3,4

---
+++ task1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "do you want to see 'hello world' whole code? 1 for yes and 0 for no" << endl;
+    int a;
+    cin >> a;
+    if (a == 1 || a == 0)
+    {
+        switch (a)
+        {
+        case 1: cout << "#include <iostream>\nint main() { \n    std::cout << \"Hello, World!\\n\" << std::endl;\n    return 0;\n}";
+        case 0: cout << "ok see you later";
+        }
+    }
+    else
+    {
+        cout << "i don't understand you";
+    }
+    return 0;
+}
---
+++ task3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int ten = 0;
+    string two, twodubl;
+    cout << "insert your binary number ";
+    cin >> two;
+    twodubl = two;
+    for (int i = 0; i < two.size(); i++)
+    {
+        twodubl[i] = (int)two[i] - 48;
+        twodubl[i] = twodubl[i] * pow(2, twodubl.size() - i - 1);
+        ten += twodubl[i];
+    }
+    cout << "answer is " << ten << endl;
+    return 0;
+}
